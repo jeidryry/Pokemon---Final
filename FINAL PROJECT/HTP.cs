f1335dab0c4c2a280950e9d513eaa00e4d7274d2
jeidryry <deriquitoivory@gmail.com>
2024-06-04 23:42:50 +0800

    Add files via upload

---
+++ FINAL PROJECT/HTP.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FINAL_PROJECT
+{
+    public partial class HTP : Form
+    {
+        public HTP()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MainMenu f1 = new MainMenu();
+            f1.Show();
+            this.Hide();
+        }
+    }
+}
